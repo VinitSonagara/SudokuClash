0161505c06c63fb593389983d1a915b188a21a64
Vinit Sonagara <vinitsonagara@gmail.com>
2024-07-17 19:06:56 +0530

    - Get code from the server when create btn is clicked.  - Create socket connection with the server.

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { io } from 'socket.io-client';
+
 const HomePage = (): React.JSX.Element => {
-    return <div>HomePage</div>;
+    const navigate = useNavigate();
+
+    const onCreateBtnClick = async () => {
+        const response = await axios.get('http://localhost:3000/create');
+        console.log({ response });
+        const socket = io('http://localhost:3000');
+        socket.on('connection', () => {
+            console.log('Connected to server:', socket.id);
+        });
+    };
+
+    const onJoinBtnClick = () => {
+        navigate('/join');
+    };
+
+    return (
+        <>
+            <button onClick={onCreateBtnClick}>Create Game</button>
+            <button onClick={onJoinBtnClick}>Join Game</button>
+        </>
+    );
 };
 
 export default HomePage;
